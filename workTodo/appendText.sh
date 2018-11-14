8ebd65ba0d1b34ff6ca63614a7362fa5a6029a13
Dan Flory <dflory@jackhenry.com>
2018-11-14 08:48:29 -0600

    add last lines to .ol

--- workTodo/appendText.sh
+++ workTodo/appendText.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-for i in *gtd.ol.txt
+for i in *.ol.txt
 do
    echo $i
-   #echo "YOUR Done next do item will delete project .ol. file\nlast line" >> $i
+   echo "YOUR Done next do item will delete project .ol. file" >> $i
+   echo "last line" >> $i
+
 done
