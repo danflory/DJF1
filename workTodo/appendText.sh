8a146a0146ae13e3bf966382e21edb22039fe96f
Dan Flory <dflory@jackhenry.com>
2018-11-15 12:22:55 -0600

    t

--- workTodo/appendText.sh
+++ workTodo/appendText.sh
@@ -2,7 +2,7 @@
 for i in *.ol.txt
 do
    echo $i
-   echo "YOUR Done next do item will delete project .ol. file" >> $i
-   echo "last line" >> $i
+   # echo "YOUR Done next do item will delete project .ol. file" >> $i
+   printf "last line\n" >> $i
 
 done
